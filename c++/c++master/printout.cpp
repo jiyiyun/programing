1f00a8a34291db3758830af0e66cbe85518a53a5
root <jiyiyun@gmail.com>
2017-02-14 15:03:16 +0800

    c++

---
+++ c++/c++master/printout.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << 2009 << endl;
+	cout << 3.14 << endl;
+	cout << 23 << endl;
+        cout << 'a' << endl;
+	cout << "hello world~" << endl;
+        return 0;
+}

bda6eb28ff84eabe23fa59ac9e5f26dccc034c19
root <jiyiyun@gmail.com>
2017-02-14 15:03:58 +0800

    java

---
+++ java/how_to_program/part2_Introduction_to_java_Applications/Welcome1.java
@@ -0,0 +1,9 @@
+//fig 2.1 Welcome.java
+public class Welcome1
+{
+	//main method begins execution of Java application
+	public static void main (String[] args)
+	{
+		system.out.printf("welcome to java Programming!")
+	} //end method main
+} //end class welcome1
---
+++ java/starter_to_master/part3_java_basic/hellojava.java
@@ -0,0 +1,6 @@
+public class HelloJava(
+    private static String say "learning java";
+    public static void main(String[] args)(
+        System.out.println("Hello Java" + say);    
+    )
+)

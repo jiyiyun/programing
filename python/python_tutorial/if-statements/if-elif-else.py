497e6509a1c6ec39e44a761e8dc49d102e42ced1
rich <richard818@qq.com>
2017-05-06 19:56:44 +0800

    python

---
+++ python/python_tutorial/if-statements/if-elif-else.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#if elif else test
+x=raw_input("input a number")
+if x < 0:
+    print "x < 0"
+elif x == 0:
+    print "x = 0"
+elif x > 0:
+    print "x > 0"
+else:
+    print "x is not number"
---
+++ python/python_tutorial/if-statements/if-statements.py
@@ -0,0 +1,10 @@
+x = int(raw_input("Please enter an integer:"))
+if x < 0:
+    x=0
+    print "set x to zero"
+elif x == 0:
+    print "x=0"
+elif x == 1:
+    print "x =1"
+else:
+    print "More"

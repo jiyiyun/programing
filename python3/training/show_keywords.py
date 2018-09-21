34f54b0d62fb2351aaa1f02105a27d8208e0b2f7
jiyiyun <jiyiyun@gmail.com>
2018-09-21 23:11:41 +0800

    show keyword

---
+++ python3/training/show_keywords.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+>>> import keyword
+>>> dir(keyword)
+['__all__', '__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__', 'iskeyword', 'kwlist', 'main']
+>>>
+"""
+
+import keyword
+
+print(keyword.kwlist)
